7ca515878d79043597840fd0ce4efee07c63fe3d
David Molineus <david.molineus@netzmacht.de>
2021-10-01 13:28:37 +0200

    Make it a bundle

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the NewsSorting Bundle.
+ *
+ * (c) inspiredminds
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace NewsSortingBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
+use NewsSortingBundle\NewsSortingBundle;
+
+/**
+ * Plugin for the Contao Manager.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(NewsSortingBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class, ContaoNewsBundle::class])
+                ->setReplace(['news_sorting'])
+        ];
+    }
+}
---
+++ src/NewsSortingBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the NewsSorting Bundle.
+ *
+ * (c) inspiredminds
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace NewsSortingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NewsSortingBundle extends Bundle
+{
+
+}
