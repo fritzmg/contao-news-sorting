caa4119b881adfc4b93dfd12ce2005bb78750805
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2026-03-05 10:28:33 +0100

    Use array notation

--- contao/dca/tl_news.php
+++ contao/dca/tl_news.php
@@ -12,5 +12,5 @@ declare(strict_types=1);
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['sorting'] = [
     'sorting' => true,
-    'sql' => 'int(10) unsigned NOT NULL default 0',
+    'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0],
 ];
